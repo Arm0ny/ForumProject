e207451fdeace9a4795e19bfc833116e513474ce
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2023-02-20 13:41:46 +0100

    changed some colors and made some design modifications

--- forum-frontend/tailwind.config.js
+++ forum-frontend/tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
       },
     },
 
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.625rem",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+    },
+
     extend: {},
   },
 
