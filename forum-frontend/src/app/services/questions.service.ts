60f2adc7b1dac8bf989f93bcaa4e4d0914d2abb6
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2023-02-23 17:51:11 +0100

    made sidebar style proper on large screen

--- forum-frontend/src/app/services/questions.service.ts
+++ forum-frontend/src/app/services/questions.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
 import { QuestionsInterface } from '../interfaces/questionsInterface';
 import { AuthService } from './auth/auth.service';
 import { ApiResponseInterface } from '../interfaces/api-response-interface';
+import { CategoriesInterface } from '../interfaces/categories-interface';
 @Injectable({
   providedIn: 'root',
 })
 export class QuestionsService {
   baseUrl = 'http://127.0.0.1:8000/api/questions';
   private categoryId?: number;
+  private categorySubject = new BehaviorSubject<string>('');
+  selectedCategory = this.categorySubject.asObservable();
 
   constructor(private http: HttpClient, private authService: AuthService) {}
-
   getQuestions(
     page: string = '',
     category = this.categoryId
@@ -24,20 +26,6 @@ export class QuestionsService {
     const params = new HttpParams().set('cursor', page);
     return this.http.get<ApiResponseInterface>(url, { params });
   }
-
-  /* getByCategory(
-    category_id: number | null = null,
-    page = ''
-  ): Observable<ApiResponseInterface> {
-    if (!category_id) {
-      return this.getQuestions();
-    }
-    const params = new HttpParams().set('cursor', page);
-    return this.http.get<ApiResponseInterface>(
-      this.baseUrl + `/category/${category_id}`,
-      { params }
-    );
-  } */
 
   store(
     title: string,
